a7f022046c7689cf5356e835beb877174701e227
Abhishek Rao <abhishekraoh410@gmail.com>
2026-06-21 09:51:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
+
 app.get("/", (req, res) => {
   res.send("🚀 CampusPulse Backend Running Successfully");
 });
